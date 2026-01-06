b969ac685e494a09f411bed94a866610992856f3
Seraaj De Villiers <seraajdevilliers@gmail.com>
2026-01-06 12:35:24 +0200

    update ui

--- app/(shared-layout)/blog/page.tsx
+++ app/(shared-layout)/blog/page.tsx
@@ -77,22 +77,3 @@ async function LoadBlogList() {
     </div>
   );
 }
-
-function SkeletonLoadingUi() {
-  return (
-    <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-      {[...Array(3)].map((_, index) => (
-        <div
-          key={index}
-          className="flex flex-col space-y-3">
-          <Skeleton className="h-48 w-full rounded-xl" />
-          <div className="space-y-2 flex flex-col">
-            <Skeleton className="h-6 w-3/4" />
-            <Skeleton className="h-4 w-full" />
-            <Skeleton className="h-4 w-2/3" />
-          </div>
-        </div>
-      ))}
-    </div>
-  );
-}
